517f24ec7a54550932d6f6882423b54c56fdaa6c
adharsh <adharshbaswaraj@gmail.com>
2026-04-02 23:51:52 +0530

    Revert "Auth: route protection middleware"

--- server/src/server.js
+++ server/src/server.js
@@ -7,14 +7,6 @@ const PORT = process.env.PORT;
 // connect DB first
 connectDB();
 
-const { generateToken, verifyToken } = require('./utils/jwt');
-
-const token = generateToken({ userId: '123', role: 'USER' });
-console.log('TOKEN:', token);
-
-const decoded = verifyToken(token);
-console.log('DECODED:', decoded);
-
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 });
