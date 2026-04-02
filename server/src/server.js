47d198fb48888da2d66932594176cfcde49ee56b
Adharsh <adharshbaswaraj@gmail.com>
2026-04-02 23:42:04 +0530

    feat: add JWT auth middleware to protect routes

--- server/src/server.js
+++ server/src/server.js
@@ -7,6 +7,14 @@ const PORT = process.env.PORT;
 // connect DB first
 connectDB();
 
+const { generateToken, verifyToken } = require('./utils/jwt');
+
+const token = generateToken({ userId: '123', role: 'USER' });
+console.log('TOKEN:', token);
+
+const decoded = verifyToken(token);
+console.log('DECODED:', decoded);
+
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 });
