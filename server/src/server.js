acd8b9372a6fe3645edbab5734611d92d794e71a
Adharsh <adharshbaswaraj@gmail.com>
2026-04-03 13:29:59 +0530

    feat: implement auto cancellation for no-show seat bookings

--- server/src/server.js
+++ server/src/server.js
@@ -1,11 +1,13 @@
 require('dotenv').config();
 const app = require('./app');
 const connectDB = require('./config/db');
+const startCronJobs = require('./utils/cron');
 
 const PORT = process.env.PORT;
 
 // connect DB first
 connectDB();
+startCronJobs();
 
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
