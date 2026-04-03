5d3a7f74287e2030f864325d0c79a63d3f23831d
Adharsh <adharshbaswaraj@gmail.com>
2026-04-03 11:44:55 +0530

    feat: implement login API with JWT token generation

--- server/src/controllers/auth.controller.js
+++ server/src/controllers/auth.controller.js
@@ -1,5 +1,5 @@
 const asyncHandler = require('../utils/asyncHandler');
-const { registerUser } = require('../services/auth.service');
+const { registerUser, loginUser } = require('../services/auth.service');
 
 const register = asyncHandler(async (req, res) => {
     const user = await registerUser(req.body);
@@ -10,6 +10,16 @@ const register = asyncHandler(async (req, res) => {
     });
 });
 
+const login = asyncHandler(async (req, res) => {
+    const data = await loginUser(req.body);
+
+    res.status(200).json({
+        success: true,
+        data,
+    });
+});
+
 module.exports = {
     register,
+    login
 };
--- server/src/services/auth.service.js
+++ server/src/services/auth.service.js
@@ -1,4 +1,5 @@
 const User = require('../models/user.model');
+const { generateToken } = require('../utils/jwt');
 
 const registerUser = async (data) => {
     const { name, email, password } = data;
@@ -20,6 +21,38 @@ const registerUser = async (data) => {
     };
 };
 
+const loginUser = async ({ email, password }) => {
+    const user = await User.findOne({ email });
+    if (!user) {
+        const error = new Error('Invalid credentials');
+        error.statusCode = 401;
+        throw error;
+    }
+
+    const isMatch = await user.comparePassword(password);
+    if (!isMatch) {
+        const error = new Error('Invalid credentials');
+        error.statusCode = 401;
+        throw error;
+    }
+
+    const token = generateToken({
+        userId: user._id,
+        role: user.role,
+    });
+
+    return {
+        token,
+        user: {
+            id: user._id,
+            name: user.name,
+            email: user.email,
+            role: user.role,
+        },
+    };
+};
+
 module.exports = {
     registerUser,
+    loginUser,
 };
